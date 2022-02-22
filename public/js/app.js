be4c138a5c288c8f5eaa0d2ace5d71c6eb4bf6a9
Francesco <scovascova@gmail.com>
2022-02-22 12:46:59 +0100

    pacchetti installati

--- public/js/app.js
+++ public/js/app.js
@@ -37335,8 +37335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Francesco\Desktop\Boolean\Lezione-78_17-02-22_RELAZIONI-ONE-TO-MANY\laravel-one-to-many\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Francesco\Desktop\Boolean\Lezione-78_17-02-22_RELAZIONI-ONE-TO-MANY\laravel-one-to-many\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Francesco\Desktop\Boolean\Lezione-80_21-02-22_LARAVEL-MANY-TO-MANY\laravel-many-to-many\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Francesco\Desktop\Boolean\Lezione-80_21-02-22_LARAVEL-MANY-TO-MANY\laravel-many-to-many\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
